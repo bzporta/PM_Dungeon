756bf82ba345a1ba5086097eb8c558ea7efdb1a8
Bjarne Zaremba <bjarne.zaremba@gmail.com>
2023-06-21 16:53:14 +0200

    Testsuite angepasst

--- game/test/AufgabeTestSuit.java
+++ game/test/AufgabeTestSuit.java
@@ -1,3 +1,4 @@
+import ecs.components.ai.idle.ProtectTileRadiusWalkTest;
 import ecs.entities.monster.MonsterTest;
 import ecs.entities.trap.TrapTest;
 import ecs.quests.QuestTest;
@@ -9,14 +10,14 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-        // Hier kommen alle Testklassen rein
         MonsterTest.class,
         TrapTest.class,
         QuestTest.class,
         QuestMenuTest.class,
-        QuestSystemTest.class
+        QuestSystemTest.class,
+        ProtectTileRadiusWalkTest.class
 })
 
 public class AufgabeTestSuit {
-    // bleibt leer!!!
+
 }
