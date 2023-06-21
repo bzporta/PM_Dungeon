7a1fca1d538d3dcdb8b17e3ee2c5c162add542e4
Bjarne Zaremba <bjarne.zaremba@gmail.com>
2023-06-21 11:59:27 +0200

    Grundstruktur für Test von Monster und ProtectTileRadiusWalk angelegt

---
+++ game/test/ecs/components/ai/idle/ProtectTileRadiusWalkTest.java
@@ -0,0 +1,62 @@
+package ecs.components.ai.idle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProtectTileRadiusWalkTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testNegativeBreakTime() {
+
+    }
+
+    @Test
+    public void testZeroBreakTime() {
+
+    }
+
+    @Test
+    public void testPositiveBreakTime() {
+
+    }
+
+    @Test
+    public void testMaxBreakTime() {
+
+    }
+
+    @Test
+    public void testNegativeRadius() {
+
+    }
+
+    @Test
+    public void testZeroRadius() {
+
+    }
+
+    @Test
+    public void testPositiveRadius() {
+
+    }
+
+    @Test
+    public void testMaxRadius() {
+
+    }
+
+    @Test
+    public void testNotAccessibleTile() {
+
+    }
+
+    @Test
+    public void testAccessibleTile() {
+
+    }
+}
---
+++ game/test/ecs/entities/monster/MonsterTest.java
@@ -0,0 +1,72 @@
+package ecs.entities.monster;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MonsterTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testMonsterParameters() {
+
+    }
+
+    @Test
+    public void testMonsterAdded() {
+
+    }
+
+    @Test
+    public void testNegativeKnockbackAmount() {
+
+    }
+
+    @Test
+    public void testZeroKnockbackAmount() {
+
+    }
+
+    @Test
+    public void testPositiveKnockbackAmount() {
+
+    }
+
+    @Test
+    public void testMaxKnockbackAmount() {
+
+    }
+
+    @Test
+    public void testSetFrozen() {
+
+    }
+
+    @Test
+    public void testOnDeath() {
+
+    }
+
+    @Test
+    public void testNegativeSetHitDamage() {
+
+    }
+
+    @Test
+    public void testZeroSetHitDamage() {
+
+    }
+
+    @Test
+    public void testPositiveSetHitDamage() {
+
+    }
+
+    @Test
+    public void testMaxSetHitDamage() {
+
+    }
+}
