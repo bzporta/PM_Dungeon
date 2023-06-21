6653f87e6954a9bc599e69aad11ac64638e6e039
Bjarne Zaremba <bjarne.zaremba@gmail.com>
2023-06-21 16:45:33 +0200

    Javadoc für QuestMenuTest hinzugefügt

--- game/test/graphic/QuestMenuTest.java
+++ game/test/graphic/QuestMenuTest.java
@@ -10,17 +10,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+/** Testclass for the class "QuestMenu" */
 public class QuestMenuTest {
 
     QuestMenu<Actor> questMenu;
     static String screenText;
     static boolean visible;
+
+    /** Setup for the tests
+     * A Questmenu is created and its visibility is set to "false" */
     @Before
     public void setup(){
         questMenu = mock(QuestMenuStub.class);
         visible = false;
     }
 
+    /** Test for the method "showMenu"
+     * The method is called and the visibility is set to "true"
+     * The test is passed if the visibility is "true" */
     @Test
     public void testShowMenu(){
         doCallRealMethod().when(questMenu).showMenu();
@@ -29,6 +36,9 @@ public class QuestMenuTest {
         assertTrue(visible);
     }
 
+    /** Test for the method "hideMenu"
+     * The method is called and the visibility is set to "false"
+     * The test is passed if the visibility is "false" */
     @Test
     public void testHideMenu(){
         doCallRealMethod().when(questMenu).hideMenu();
@@ -39,6 +49,9 @@ public class QuestMenuTest {
         assertFalse(visible);
     }
 
+    /** Test for the method "addActiveQuest"
+     * The method is called and the screenText is set to the name and status of the quest
+     * The test is passed if the screenText is equal to the name and status of the quest */
     @Test
     public void testAddActiveQuest(){
         doCallRealMethod().when(questMenu).addActiveQuest(anyString(), anyString());
@@ -48,6 +61,9 @@ public class QuestMenuTest {
         assertEquals("KillQuest 0/10", screenText);
     }
 
+    /** Test for the method "decreaseQuestcounter"
+     * The method is called and the questcounter is decreased by 1
+     * The test is passed if the questcounter is equal to 0 */
     @Test
     public void testDecreaseQuestCounter(){
         doCallRealMethod().when(questMenu).decreaseQuestcounter();
@@ -57,18 +73,26 @@ public class QuestMenuTest {
         assertEquals(0 ,questMenu.getQuestcounter());
     }
 
+
+    /** Stub for the class "QuestMenu" */
     public class QuestMenuStub<T extends Actor> extends QuestMenu{
 
+        /** Hides the QuestMenu */
         @Override
         public void hideMenu(){
             QuestMenuTest.visible = false;
         }
 
+        /** Shows the QuestMenu */
         @Override
         public void showMenu(){
             QuestMenuTest.visible = true;
         }
 
+        /** Adds an active quest to the QuestMenu
+         * @param name name of the quest
+         * @param status status of the quest
+         * @return null */
         @Override
         public ScreenText addActiveQuest(String name, String status){
             questcounter++;
