03d430081e6c26d000a9f6c1175b890d43590b4e
Bjarne Zaremba <bjarne.zaremba@gmail.com>
2023-06-21 16:39:26 +0200

    Javadoc für QuestTest erstellt

--- game/test/ecs/quests/QuestTest.java
+++ game/test/ecs/quests/QuestTest.java
@@ -7,15 +7,25 @@ import starter.Game;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
+/** Testclass for the classes "KillQuest" and "GraveQuest" */
 public class QuestTest {
     KillQuest killQuest;
     GraveQuest graveQuest;
+
+    /** Setup for the tests
+     *
+     * A killQuest and a graveQuest are created
+     */
     @Before
     public void setUp() throws Exception {
         killQuest = new KillQuest();
         graveQuest = new GraveQuest();
     }
 
+    /** Test for the method "countKilledMonsters" in the class "KillQuest"
+     *
+     * Testcase: The amount of killed monsters is counted
+     */
     @Test
     public void testCountKilledMonsters(){
         int count = killQuest.getKilledMonsters();
@@ -23,6 +33,10 @@ public class QuestTest {
         assertTrue(killQuest.getKilledMonsters() == count + 1);
     }
 
+    /** Test for the method "countActivatedGraves" in the class "GraveQuest"
+     *
+     * Testcase: The amount of activated graves is counted
+     */
     @Test
     public void testActivatedGraves(){
         int count = graveQuest.getActivatedGraves();
@@ -30,6 +44,10 @@ public class QuestTest {
         assertTrue(graveQuest.getActivatedGraves() == count + 1);
     }
 
+    /** Test for the method "checkIfDone" in the classes "KillQuest" and "GraveQuest"
+     *
+     * Testcase: The quest is done (The amount of killed monsters/activated graves is 10)
+     */
     @Test
     public void testCheckIfDone() {
         for (int i = 0; i < 10; i++) {
@@ -43,6 +61,10 @@ public class QuestTest {
         assertTrue(graveQuest.checkIfDone());
     }
 
+    /** Tests if the quests are in the questList
+     *
+     * Testcase: The quests are in the questList
+     */
     @Test
     public void testQuestInList(){
         Game.getQuestList().add(killQuest);
