4fa0effaeba258a1650d879dbbb5d7e5cf30cd1b
Bjarne Zaremba <bjarne.zaremba@gmail.com>
2023-06-21 17:02:30 +0200

    ProtectTileRadiusWalk angefangen

--- game/test/ecs/components/ai/idle/ProtectTileRadiusWalkTest.java
+++ game/test/ecs/components/ai/idle/ProtectTileRadiusWalkTest.java
@@ -1,17 +1,31 @@
 package ecs.components.ai.idle;
 
+import ecs.components.ai.AIComponent;
+import ecs.entities.Entity;
+import ecs.entities.Hero;
+import level.elements.ILevel;
+import level.elements.tile.Tile;
+import level.elements.tile.WallTile;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import starter.Game;
 
 public class ProtectTileRadiusWalkTest {
 
+    Hero hero;
     @Before
     public void setUp() throws Exception {
+        ILevel level = Mockito.mock(ILevel.class);
+        Game.currentLevel = level;
+        hero = new Hero();
 
     }
 
     @Test
     public void testNegativeBreakTime() {
+        AIComponent aiComponent = new AIComponent(hero, );
+        ProtectTileRadiusWalk protectTileRadiusWalk = new ProtectTileRadiusWalk(Game.currentLevel.getEndTile(), -1);
 
     }
 
